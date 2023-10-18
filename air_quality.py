5544a87c7fe2d702de6f1dbbe1c82f980229d074
ja1073 <147076985+ja1073@users.noreply.github.com>
2023-10-18 12:56:32 +0100

    Update air_quality.py

--- air_quality.py
+++ air_quality.py
@@ -75,6 +75,7 @@ def get_image(path:str)->Image:
     return image
 
 image = get_image("LBTH_banner.PNG") # path of the file
+st.image(image)
 
 st.subheader('Nitrogen dioxide (NO2)')
 st.write('''Nitrogen dioxide (NO2) is a gas that is mainly produced during the combustion of fossil fuels, along with nitric oxide (NO).
